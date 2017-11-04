1576e3ce12b6dbd4511a3ac55793209a1befaea6
EvaJamen <jamen.eva@gmail.com>
2017-11-04 14:35:20 +0100

    :tada: Initial commit : Routing

---
+++ views/partials/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Mon portfolio</title>
+	<link rel="stylesheet" href="<?= URL ?>assets/style/main.css">
+</head>
+<body>
+	<header><h1>Mon Portfolio</h1></header>
